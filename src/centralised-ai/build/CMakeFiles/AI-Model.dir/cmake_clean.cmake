36e36f3d7077e2f93cee4c2037eb88426d93f6d3
Ven20002 <ven20002@student.mdu.se>
2024-10-23 10:20:51 +0200

    Setup, not done, just merged and fixed in some cmake files

---
+++ src/centralised-ai/build/CMakeFiles/AI-Model.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "AI-Model"
+  "AI-Model.pdb"
+  "CMakeFiles/AI-Model.dir/main.cc.o"
+  "CMakeFiles/AI-Model.dir/main.cc.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AI-Model.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
