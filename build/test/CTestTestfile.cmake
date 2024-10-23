6d7d47a6af59b85d062936a1184043737b096f51
Emil Åberg <emilgamma@gmail.com>
2024-10-23 14:34:32 +0200

    ..remove another build folder

--- build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vicore/Git/centralised-ai/test
-# Build directory: /home/vicore/Git/centralised-ai/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/vicore/Git/centralised-ai/build/test/main_test_exe[1]_include.cmake")
-subdirs("../_deps/googletest-build")
