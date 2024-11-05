cdacbce15000df9a4b0104c92506d69612687f68
Jacob <jjn20030@student.mdu.se>
2024-11-05 12:03:40 +0100

    Added functions for communication.

--- src/collective-robot-behaviour/communication.cc
+++ src/collective-robot-behaviour/communication.cc
@@ -23,14 +23,6 @@ namespace collective_robot_behaviour
     int32_t robot_id;
   };
 
-  struct RewardConfiguration
-  {
-    float average_distance_reward;
-    float have_ball_reward;
-
-    float max_distance_from_center;
-  };
-
   static int32_t ComputeGoalDifference(ssl_interface::AutomatedReferee referee, Team team)
   {
     switch (team)
@@ -76,15 +68,83 @@ namespace collective_robot_behaviour
     return have_ball;
   }
 
-  State GetObservations(ssl_interface::AutomatedReferee referee, ssl_interface::VisionClient vision_client, Team team)
+  torch::Tensor GetStates(ssl_interface::AutomatedReferee & referee, ssl_interface::VisionClient & vision_client, Team own_team, Team opponent_team)
   {
-    /* Cache the current state of the game.*/
-    referee.AnalyzeGameState();
+    torch::Tensor states = torch::empty(30);
+    states[0] = 0; /* Reserved for the robot id. */
 
-    int32_t num_inputs = 34;
-    int32_t num_robots = 6;
-    torch::Tensor state_vector = torch::empty({1,1,num_inputs});
+    /* Ball position */
+    states[1] = vision_client.GetBallPositionX();
+    states[2] = vision_client.GetBallPositionY();
 
+    /* own team positions */
+    states[3] = vision_client.GetRobotPositionX(0, own_team);
+    states[4] = vision_client.GetRobotPositionY(0, own_team);
+    states[5] = vision_client.GetRobotPositionX(1, own_team);
+    states[6] = vision_client.GetRobotPositionY(1, own_team);
+    states[7] = vision_client.GetRobotPositionX(2, own_team);
+    states[8] = vision_client.GetRobotPositionY(2, own_team);
+    states[9] = vision_client.GetRobotPositionX(3, own_team);
+    states[10] = vision_client.GetRobotPositionY(3, own_team);
+    states[11] = vision_client.GetRobotPositionX(4, own_team);
+    states[12] = vision_client.GetRobotPositionY(4, own_team);
+    states[13] = vision_client.GetRobotPositionX(5, own_team);
+    states[14] = vision_client.GetRobotPositionY(5, own_team);
+
+    /* opponent team positions */
+    states[15] = vision_client.GetRobotPositionX(0, opponent_team);
+    states[16] = vision_client.GetRobotPositionY(0, opponent_team);
+    states[17] = vision_client.GetRobotPositionX(1, opponent_team);
+    states[18] = vision_client.GetRobotPositionY(1, opponent_team);
+    states[19] = vision_client.GetRobotPositionX(2, opponent_team);
+    states[20] = vision_client.GetRobotPositionY(2, opponent_team);
+    states[21] = vision_client.GetRobotPositionX(3, opponent_team);
+    states[22] = vision_client.GetRobotPositionY(3, opponent_team);
+    states[23] = vision_client.GetRobotPositionX(4, opponent_team);
+    states[24] = vision_client.GetRobotPositionY(4, opponent_team);
+    states[25] = vision_client.GetRobotPositionX(5, opponent_team);
+    states[26] = vision_client.GetRobotPositionY(5, opponent_team);
+
+    /* Goal difference */
+    states[27] = ComputeGoalDifference(referee, own_team);
+
+    /* Ball owner */
+    BallOwner ball_owner = ComputeBallOwner(referee, own_team, opponent_team, 6);
+    states[28] = ball_owner.team_id;
+    states[29] = ball_owner.robot_id;
+
+    /* Remaining time */
+    states[30] = referee.GetStageTimeLeft();
+
+    return states;
+  }
+
+  torch::Tensor ComputeRewards(ssl_interface::AutomatedReferee & referee, torch::Tensor & states, RewardConfiguration reward_configuration, Team own_team)
+  {
+    torch::Tensor positions = torch::zeros({2, 6});
+    positions[0][0] = states[3];
+    positions[1][0] = states[4];
+    positions[0][1] = states[5];
+    positions[1][1] = states[6];
+    positions[0][2] = states[7];
+    positions[1][2] = states[8];
+    positions[0][3] = states[9];
+    positions[1][3] = states[10];
+    positions[0][4] = states[11];
+    positions[1][4] = states[12];
+    positions[0][5] = states[13];
+    positions[1][5] = states[14];
+
+    torch::Tensor average_distance_reward = ComputeAverageDistanceReward(positions, 1, reward_configuration.average_distance_reward);
+    torch::Tensor have_ball = ComputeHaveBall(referee, own_team, 6);
+    torch::Tensor have_ball_reward = ComputeHaveBallReward(have_ball, reward_configuration.have_ball_reward);
+    torch::Tensor total_reward = average_distance_reward + have_ball_reward;
+
+    return total_reward;
+  }
+
+  Observation GetObservations(ssl_interface::AutomatedReferee & referee, ssl_interface::VisionClient & vision_client, RewardConfiguration reward_configuration, Team team)
+  {
     /* Correct the dessignated teams.*/
     Team teammate;
     Team opponent;
@@ -107,78 +167,13 @@ namespace collective_robot_behaviour
       break;
     }
 
-    /* Compute the ball owner*/
-    BallOwner ball_owner = ComputeBallOwner(referee, teammate, opponent, num_robots);
+    /* Get the states of the world. */
+    torch::Tensor states = GetStates(referee, vision_client, teammate, opponent);
 
-    /* [0] is reservered for robot id.*/
-    /* Ball position */
-    state_vector[1] = vision_client.GetBallPositionX();
-    state_vector[2] = vision_client.GetBallPositionY();
-    /* Team positions */
-    state_vector[3] = vision_client.GetRobotPositionX(0, teammate);
-    state_vector[4] = vision_client.GetRobotPositionY(0, teammate);
-    state_vector[5] = vision_client.GetRobotPositionX(1, teammate);
-    state_vector[6] = vision_client.GetRobotPositionY(1, teammate);
-    state_vector[7] = vision_client.GetRobotPositionX(2, teammate);
-    state_vector[8] = vision_client.GetRobotPositionY(2, teammate);
-    state_vector[9] = vision_client.GetRobotPositionX(3, teammate);
-    state_vector[10] = vision_client.GetRobotPositionY(3, teammate);
-    state_vector[11] = vision_client.GetRobotPositionX(4, teammate);
-    state_vector[12] = vision_client.GetRobotPositionY(4, teammate);
-    state_vector[13] = vision_client.GetRobotPositionX(5, teammate);
-    state_vector[14] = vision_client.GetRobotPositionY(5, teammate);
-    /* Opponent positions */
-    state_vector[15] = vision_client.GetRobotPositionX(0, opponent);
-    state_vector[16] = vision_client.GetRobotPositionY(0, opponent);
-    state_vector[17] = vision_client.GetRobotPositionX(1, opponent);
-    state_vector[18] = vision_client.GetRobotPositionY(1, opponent);
-    state_vector[19] = vision_client.GetRobotPositionX(2, opponent);
-    state_vector[20] = vision_client.GetRobotPositionY(2, opponent);
-    state_vector[21] = vision_client.GetRobotPositionX(3, opponent);
-    state_vector[22] = vision_client.GetRobotPositionY(3, opponent);
-    state_vector[23] = vision_client.GetRobotPositionX(4, opponent);
-    state_vector[24] = vision_client.GetRobotPositionY(4, opponent);
-    state_vector[25] = vision_client.GetRobotPositionX(5, opponent);
-    state_vector[26] = vision_client.GetRobotPositionY(5, opponent);
-    /* Goal difference */
-    state_vector[27] = ComputeGoalDifference(referee, teammate);
-    /* Ball owned team */
-    state_vector[28] = ball_owner.team_id;
-    /* Robot id with ball */
-    state_vector[29] = ball_owner.robot_id;
-    /* Team yellow cards*/
-    //state_vector[30] =
-    /* Team red cards */
-    //state_vector[31] =
-    /* Opponent yellow cards */
-    //state_vector[32] =
-    /* Opponent red cards */
-    //state_vector[33] =
-    /* Time remaining */
-    state_vector[34] = referee.GetStageTimeLeft();
+    /* Compute the rewards for each agent. */
+    torch::Tensor rewards = ComputeRewards(referee, states, reward_configuration, teammate);
 
-    /* Calculate the rewards per robot. */
-    torch::Tensor positions = torch::zeros({2, num_robots});
-    positions[0][0] = state_vector[3];
-    positions[1][0] = state_vector[4];
-    positions[0][1] = state_vector[5];
-    positions[1][1] = state_vector[6];
-    positions[0][2] = state_vector[7];
-    positions[1][2] = state_vector[8];
-    positions[0][3] = state_vector[9];
-    positions[1][3] = state_vector[10];
-    positions[0][4] = state_vector[11];
-    positions[1][4] = state_vector[12];
-    positions[0][5] = state_vector[13];
-    positions[1][5] = state_vector[14];
-
-    torch::Tensor average_distance_reward = ComputeAverageDistanceReward(positions, 1, -0.0001);
-    torch::Tensor have_ball = ComputeHaveBall(referee, teammate, num_robots);
-    torch::Tensor have_ball_reward = ComputeHaveBallReward(have_ball, 0.0001);
-    torch::Tensor total_reward = average_distance_reward + have_ball_reward;
-
-
-    return State{rewards : total_reward, state : state_vector};
+    return Observation{rewards : rewards, state : states};
   }
 }/* namespace centralised_ai */
 }/*namespace collective_robot_behaviour*/
--- src/collective-robot-behaviour/communication.h
+++ src/collective-robot-behaviour/communication.h
@@ -25,11 +25,29 @@ namespace centralised_ai
 namespace collective_robot_behaviour
 {
 
-struct State
+/*!
+*@brief Struct representing the observation of the state of the world.
+*/
+struct Observation
 {
+  /* Tensor representing the rewards for each agent, with the shape [num_agents].*/
   torch::Tensor rewards;
-
+  /* Tensor representing the state of the world, with the shape [num_states]*/
   torch::Tensor state;
+};
+
+/*!
+*@brief Struct representing the configuration of the rewards.
+*/
+struct RewardConfiguration
+{
+    /* The reward that will be given to the robot when within max_distance_from_center. */
+    float average_distance_reward;
+    /* The maximum distance from the center within the robot will receive the average_distance_reward. */
+    float max_distance_from_center;
+
+    /* The reward that will be given to the robot when it has the ball. */
+    float have_ball_reward;
 };
 
 /*!
@@ -38,12 +56,71 @@ struct State
  *@pre The following preconditions must be met before using this class:
  * - A connection to grSim.
  *
- * @param[In] referee: The automated referee, which is the source of the current state of the world.
- * @param[In] vision_client: The vision client, which is the source of the current state of the world.
- * @param[In] Team: The team that the agents are on.
+ *@param[In] referee: The automated referee, which is the source of the current state of the world.
+ *@param[In] vision_client: The vision client, which is the source of the current state of the world.
+ *@param[In] Team: The team that the agents are on.
  *@param[Out] Tensor array of the current state that includes ....
  */
-State GetObservations(ssl_interface::AutomatedReferee referee, ssl_interface::VisionClient vision_client, Team team);
+Observation GetObservations(ssl_interface::AutomatedReferee referee, ssl_interface::VisionClient vision_client, RewardConfiguration reward_configuration, RewardConfiguration, Team team);
+
+/*!
+*@brief Get the current state from grSim
+*
+*@returns A tensor representing the states of the world, with the shape [num_states].
+* The states are as follows:
+* [0] - Reserved for the robot id as input to each policy network.
+* [1] - The x-coordinate of the ball.
+* [2] - The y-coordinate of the ball.
+* [3] - The x-coordinate of the teammate robot 0.
+* [4] - The y-coordinate of the teammate robot 0.
+* [5] - The x-coordinate of the teammate robot 1.
+* [6] - The y-coordinate of the teammate robot 1.
+* [7] - The x-coordinate of the teammate robot 2.
+* [8] - The y-coordinate of the teammate robot 2.
+* [9] - The x-coordinate of the teammate robot 3.
+* [10] - The y-coordinate of the teammate robot 3.
+* [11] - The x-coordinate of the teammate robot 4.
+* [12] - The y-coordinate of the teammate robot 4.
+* [13] - The x-coordinate of the teammate robot 5.
+* [14] - The y-coordinate of the teammate robot 5.
+* [15] - The x-coordinate of the opponent robot 0.
+* [16] - The y-coordinate of the opponent robot 0.
+* [17] - The x-coordinate of the opponent robot 1.
+* [18] - The y-coordinate of the opponent robot 1.
+* [19] - The x-coordinate of the opponent robot 2.
+* [20] - The y-coordinate of the opponent robot 2.
+* [21] - The x-coordinate of the opponent robot 3.
+* [22] - The y-coordinate of the opponent robot 3.
+* [23] - The x-coordinate of the opponent robot 4.
+* [24] - The y-coordinate of the opponent robot 4.
+* [25] - The x-coordinate of the opponent robot 5.
+* [26] - The y-coordinate of the opponent robot 5.
+* [27] - The goal difference.
+* [28] - The team id of the robot that owns the ball.
+* [29] - The robot id of the robot that owns the ball.
+* [30] - The time remaining in the current stage.
+*
+*@pre The following preconditions must be met before using this class:
+* - A connection to grSim.
+* - AutomatedReferee::AnalyzeGameState() and VisionClient::ReceivePacket() must be called on a separate thread.
+*
+*@param[In] referee: The automated referee, which is the source of the current state of the world.
+*@param[In] vision_client: The vision client, which is the source of the current state of the world.
+*@param[In] own_team: The team that the robots are on.
+*@param[In] opponent_team: The team that the robots are playing against.
+*/
+torch::Tensor GetStates(ssl_interface::AutomatedReferee & referee, ssl_interface::VisionClient & vision_client, Team own_team, Team opponent_team);
+
+/*!
+*@brief Computes the rewards for each agent.
+*
+*@returns A tensor representing the rewards for each agent, with the shape [num_agents].
+*@param[In] referee: The automated referee, which is the source of the current state of the world.
+*@param[In] states: The states of the world, with the shape [num_states].
+*@param[In] reward_configuration: The configuration of the rewards.
+*@param[In] own_team: The team that the agents are on.
+*/
+torch::Tensor ComputeRewards(ssl_interface::AutomatedReferee & referee, torch::Tensor & states, RewardConfiguration reward_configuration, Team own_team);
 
 }/*namespace centralised_ai*/
 }/*namespace collective_robot_behaviour*/
