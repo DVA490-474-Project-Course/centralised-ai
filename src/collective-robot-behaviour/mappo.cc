86cc4556559e639242f69ea3239c91064963e72a
Jacob <jjn20030@student.mdu.se>
2024-11-07 14:17:30 +0100

    Fixed state to be one dimensional instead of three.

--- src/collective-robot-behaviour/mappo.cc
+++ src/collective-robot-behaviour/mappo.cc
@@ -115,7 +115,7 @@ std::vector<DataBuffer> MappoRun(std::vector<Agents> Models, CriticNetwork criti
       exp.actions = actions_agents;
       exp.state = state;
       exp.criticvalues = valNetOutput.squeeze().expand({amount_of_players_in_team});
-      exp.rewards = run_state.ComputeRewards(state, {-0.00001, 0.5, 0.001}).expand({1, amount_of_players_in_team});
+      exp.rewards = run_state.ComputeRewards(state.squeeze(0).squeeze(0), {-0.00001, 0.5, 0.001}).expand({1, amount_of_players_in_team});
       state = GetStates(referee,vision_client,own_team,opponent_team);
       exp.hidden_v.ht_p = V_hx;
       exp.hidden_v.ct_p = V_cx;
