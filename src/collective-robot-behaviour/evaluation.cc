3be28f27700824fde16e552a962e8534dcb515da
Jacob <jjn20030@student.mdu.se>
2024-12-05 15:08:17 +0100

    Added loading reward to go from file.

--- src/collective-robot-behaviour/evaluation.cc
+++ src/collective-robot-behaviour/evaluation.cc
@@ -2,6 +2,8 @@
 #include "evaluation.h"
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <vector>
 
 namespace centralised_ai
 {
@@ -36,6 +38,53 @@ namespace collective_robot_behaviour
         }
 
         file.close();
+    }
+
+    torch::Tensor LoadRewardToGoFromFile(const std::string & filename)
+    {
+        /* Load the reward to go tensor from a file. */
+        std::ifstream file(filename, std::ios::in);
+
+        if(!file.is_open())
+        {
+            std::cerr << "Could not open file: " << filename << std::endl;
+            return torch::zeros({0, 0});
+        }
+
+        /* Create tensor with the shape [num_columns, num_rows]. */
+        std::vector<std::vector<float>> reward_to_go_data;
+        std::string line;
+        
+        while (std::getline(file, line))
+        {
+            std::istringstream iss(line);
+            std::vector<float> reward_to_go_row;
+            std::string value;
+            while (std::getline(iss, value, ','))
+            {
+                reward_to_go_row.push_back(std::stof(value));
+            }
+
+            reward_to_go_data.push_back(reward_to_go_row);
+        }
+
+        file.close();
+
+        /* Create a new tensor from the vector of vectors. */
+        int32_t num_columns = reward_to_go_data.size();
+        int32_t num_rows = reward_to_go_data[0].size();
+
+        torch::Tensor reward_to_go = torch::zeros({num_columns, num_rows});
+
+        for (int32_t c = 0; c < num_columns; c++)
+        {
+            for (int32_t r = 0; r < num_rows; r++)
+            {
+                reward_to_go[c][r] = reward_to_go_data[c][r];
+            }
+        }
+
+        return reward_to_go;
     }
 }
 }
--- src/collective-robot-behaviour/evaluation.h
+++ src/collective-robot-behaviour/evaluation.h
@@ -11,11 +11,19 @@ namespace centralised_ai
 namespace collective_robot_behaviour
 {
     /*!
-    *@brief Save the reward_to_go to a file.
+    *@brief Saves the reward_to_go to a file.
     * @param reward_to_go: The reward to go tensor, with the shape [batch_size, num_time_steps].
     * @param filename: The filename to save the reward to go to.
     */
     void SaveRewardToGoToFile(const torch::Tensor & reward_to_go, const std::string & filename);
+
+    /*!
+    *@brief Loads the reward to go from a file.
+    * @param filename: The filename to load the reward to go from.
+    * @returns The reward to go tensor, with the shape [batch_size * num_batches, num_time_steps].
+    * NOTE: Num batches is the total number of batches that are in the file, i.e. batches from all episodes.
+    */
+    torch::Tensor LoadRewardToGoFromFile(const std::string & filename);
 }
 }
 
